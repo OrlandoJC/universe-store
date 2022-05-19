5146be5d53a205583a331c7862e074928c8b7eb5
orlandoJc <cuximyamaorlandodejesus@gmail.com>
2022-05-18 23:09:01 -0500

    añadido cartWidget e itemListContainer

---
+++ src/components/CartWidget.js
@@ -0,0 +1,14 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCartShopping } from '@fortawesome/free-solid-svg-icons'
+
+const CartWidget = ({items}) => {
+    return (
+        <div className="cart" id="cart">
+            <div>
+                <FontAwesomeIcon icon={faCartShopping} /> Carrito: <span id="cart__counter">{items}</span>
+            </div>
+        </div>
+    )
+}
+
+export default CartWidget
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,25 +1,21 @@
-import React from 'react'
+import CartWidget from './CartWidget'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faMagnifyingGlass, faCartShopping } from '@fortawesome/free-solid-svg-icons'
+import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons'
 
 const Navbar = () => {
     return (
         <header className="header">
             <div className="header__container container">
                 <div className="logo">
-                <img src="https://labrocheta.netlify.app/assets/img/icons/logo.png" alt="" width="50px" />
+                    <img src="https://labrocheta.netlify.app/assets/img/icons/logo.png" alt="" width="50px" />
                 </div>
 
                 <div className="header__group">
                     <div className="header__bar">
                         <input className="hedaer__finder" id="search" type="text" placeholder="Buscar.." />
-                        <button className="bar__search"><FontAwesomeIcon icon={faMagnifyingGlass}/></button>
+                        <button className="bar__search"><FontAwesomeIcon icon={faMagnifyingGlass} /></button>
                     </div>
-                    <div className="cart" id="cart">
-                        <div>
-                        <FontAwesomeIcon icon={faCartShopping}/> Carrito: <span id="cart__counter">0</span>
-                        </div>
-                    </div>
+                    <CartWidget items={4} />
                 </div>
             </div>
         </header>
